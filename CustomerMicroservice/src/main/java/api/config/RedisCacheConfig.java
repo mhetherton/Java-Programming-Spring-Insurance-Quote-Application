74cd297503ef4024711d91dc0c0514f8093937a7
mhetherton <michael.hetherton@gmail.com>
2026-06-13 17:02:03 +1000

    fixed redis to not display error warnings when not available.

--- CustomerMicroservice/src/main/java/api/config/RedisCacheConfig.java
+++ CustomerMicroservice/src/main/java/api/config/RedisCacheConfig.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurer;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.LoggingCacheErrorHandler;
 import org.springframework.context.annotation.Bean;
@@ -43,6 +42,6 @@ public class RedisCacheConfig implements CachingConfigurer {
     @Override
     public CacheErrorHandler errorHandler() {
         // Automatically catches Redis errors, logs them, and bypasses the cache
-        return new LoggingCacheErrorHandler();
+        return new LoggingCacheErrorHandler(false); // Set to false to avoid logging stack traces for cache
     }
 }
